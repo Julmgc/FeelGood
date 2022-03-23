badc6ca4f23fb5620cbcd61c40a575298cb99106
Divinity <systemdivinity@gmail.com>
2022-03-23 13:39:47 -0300

    feat/model-address

---
+++ address/models.py
@@ -0,0 +1,14 @@
+from uuid import uuid4
+from django.db import models
+
+# Create your models here.
+
+class Address(models.Model):
+    id = models.UUIDField(editable=False, default=uuid4, primary_key=True)
+    street = models.CharField(max_length=30)
+    cep = models.CharField(max_length=9)
+    house_number = models.CharField(max_length=5)
+    city = models.CharField(max_length=20)
+    district = models.CharField(max_length=30)
+    state = models.CharField(max_length=2)
+    country = models.CharField(max_length=30)
