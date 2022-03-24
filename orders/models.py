f65a055c88cf68c1b483f9cc9932deb2ea988497
André Luiz S. Vilarinho <andluizv@msn.com>
2022-03-23 21:46:23 -0300

    feat: order model

--- orders/models.py
+++ orders/models.py
@@ -5,7 +5,7 @@ from sinta_se_bem.base_classes import GenericWithUUID
 
 class Order(GenericWithUUID):
 
-    quantity = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField(default=0)
     amount = models.DecimalField(decimal_places=2, max_digits=19)
     product = models.ForeignKey('products.Product', related_name='orders', on_delete=models.PROTECT)
     transaction = models.ForeignKey('transactions.Transaction', related_name='orders', on_delete=models.PROTECT)
