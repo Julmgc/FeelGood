c352f110ad68eef2fda512fd4ace8530196358b3
Divinity <systemdivinity@gmail.com>
2022-03-24 09:21:14 -0300

    feat/model-payment

--- payment/models.py
+++ payment/models.py
@@ -1,14 +1,12 @@
-from uuid import uuid4
 from django.db import models
-
+from sinta_se_bem.base_classes import GenericWithUUID
 # Create your models here.
 
-class Payment(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+class Payment(GenericWithUUID):
     payment_method = models.CharField(max_length=20)
     card_number = models.CharField(max_length=20)
     cardholders_name = models.CharField(max_length=20)
     card_expiration_date = models.DateTimeField()
     cvv = models.CharField(max_length=20)
     is_active = models.BooleanField(default=True)
-    # costumer_id = models.
+    costumer_id = models.ForeignKey("user.User", on_delete=models.CASCADE, related_name='payment_id')
