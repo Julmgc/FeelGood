83694db828375a559ceb81f1e450d92524bcd84c
André Luiz S. Vilarinho <andluizv@msn.com>
2022-03-23 21:44:11 -0300

    feat: order model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from sinta_se_bem.base_classes import GenericWithUUID
+
+
+class Order(GenericWithUUID):
+
+    quantity = models.PositiveIntegerField()
+    amount = models.DecimalField(decimal_places=2, max_digits=19)
+    product = models.ForeignKey('products.Product', related_name='orders', on_delete=models.PROTECT)
+    transaction = models.ForeignKey('transactions.Transaction', related_name='orders', on_delete=models.PROTECT)
+    sale = models.ForeignKey('sales.Sale', related_name='orders', on_delete=models.PROTECT, null=True)
