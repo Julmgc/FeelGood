b9506adfa70b2dabeef3288b9d252a6240117fb9
Natalia Nunes <naticristinenunes@gmail.com>
2022-03-24 12:16:10 -0300

    create relationship n:n addid  product model

--- products/models.py
+++ products/models.py
@@ -11,3 +11,5 @@ class Product(GenericWithUUID):
     quantity = models.IntegerField(default=0, validators=[validators.MinValueValidator(0)])
     expiration_date = models.DateField()
     sale=models.ForeignKey('sales.Sale',on_delete=models.CASCADE,null=True)
+    course_links = models.ManyToManyField('course_link.CourseLink',related_name='course_links',null=True)
+    categories = models.ManyToManyField('category.Category',related_name='categories',null=True)
