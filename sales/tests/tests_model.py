fe526c94dbd363cdb3328769e2dcad92dff2cab3
Natalia Nunes <naticristinenunes@gmail.com>
2022-03-24 16:52:30 -0300

    fix model sale

--- sales/tests/tests_model.py
+++ sales/tests/tests_model.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, datetime
 from django.test import TestCase
 from sales.models import Sale
 
@@ -7,8 +7,8 @@ class SaleModelTest(TestCase):
     def setUpTestData(cls) -> None:
         cls.discount_percentage=0.05
         cls.is_active = True
-        cls.initial_datetime = "2022/03/25"
-        cls.final_datetime = "2022/04/02"
+        cls.initial_datetime = "2022/03/25 14:30:59"
+        cls.final_datetime = "2022/04/02 14:30:59"
 
         cls.sale = Sale.objects.create_user(
            discount_percentage = cls.discount_percentage ,
@@ -24,8 +24,8 @@ class SaleModelTest(TestCase):
         self.assertIsInstance(self.sale.is_active,bool)
         self.assertEqual(self.sale.is_active,self.sale.is_active)
 
-        self.assertIsInstance(self.sale.initial_datetime, date)
+        self.assertIsInstance(self.sale.initial_datetime, datetime)
         self.assertEqual(self.sale.initial_datetime,self.initial_datetime)
 
-        self.assertIsInstance(self.sale.final_datetime, date)
+        self.assertIsInstance(self.sale.final_datetime, datetime)
         self.assertEqual(self.sale.final_datetime,self.final_datetime)
