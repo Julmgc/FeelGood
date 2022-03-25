adeacc87e042e57e941794c7653f8fcd6a1ea4e1
DeniseBelo <denise_belo19@yahoo.com.br>
2022-03-25 11:43:17 -0300

    feat: testing models united

--- sales/models.py
+++ sales/models.py
@@ -3,9 +3,7 @@ from sinta_se_bem.base_classes import GenericWithUUID
 
 
 class Sale(GenericWithUUID):
-    dicount_percentage=models.FloatField()
+    discount_percentage = models.FloatField()
     is_active = models.BooleanField(default=True)
     initial_datetime = models.DateTimeField()
     final_datetime = models.DateTimeField()
-
-
--- sales/tests/tests_model.py
+++ sales/tests/tests_model.py
@@ -2,30 +2,34 @@ from datetime import date, datetime
 from django.test import TestCase
 from sales.models import Sale
 
+
 class SaleModelTest(TestCase):
     @classmethod
     def setUpTestData(cls) -> None:
-        cls.discount_percentage=0.05
+        cls.discount_percentage = 0.05
         cls.is_active = True
-        cls.initial_datetime = "2022-03-24T21:36:16.118950Z"
-        cls.final_datetime = "2022-04-24T21:36:16.118950Z"
+        cls.initial_datetime = datetime.strptime(
+            '07/28/2022 18:54:55.099000', '%m/%d/%Y %H:%M:%S.%f').date()
+        cls.final_datetime = datetime.strptime(
+            '07/29/2022 18:54:55.099000', '%m/%d/%Y %H:%M:%S.%f').date()
 
         cls.sale = Sale.objects.create(
-           discount_percentage = cls.discount_percentage ,
-           is_active = cls.is_active,
-           initial_datetime = cls.initial_datetime ,
-           final_datetime = cls.final_datetime 
+            discount_percentage=cls.discount_percentage,
+            is_active=cls.is_active,
+            initial_datetime=cls.initial_datetime,
+            final_datetime=cls.final_datetime
         )
-    
+
     def test_sale_field(self):
-        self.assertIsInstance(self.sale.discount_percentage,float)
-        self.assertEqual(self.sale.discount_percentage,self.discount_percentage)
+        self.assertIsInstance(self.sale.discount_percentage, float)
+        self.assertEqual(self.sale.discount_percentage,
+                         self.discount_percentage)
 
-        self.assertIsInstance(self.sale.is_active,bool)
-        self.assertEqual(self.sale.is_active,self.sale.is_active)
+        self.assertIsInstance(self.sale.is_active, bool)
+        self.assertEqual(self.sale.is_active, self.sale.is_active)
 
-        self.assertIsInstance(self.sale.initial_datetime, datetime)
-        self.assertEqual(self.sale.initial_datetime,self.initial_datetime)
+        self.assertIsInstance(self.sale.initial_datetime, date)
+        self.assertEqual(self.sale.initial_datetime, self.initial_datetime)
 
-        self.assertIsInstance(self.sale.final_datetime, datetime)
-        self.assertEqual(self.sale.final_datetime,self.final_datetime)
+        self.assertIsInstance(self.sale.final_datetime, date)
+        self.assertEqual(self.sale.final_datetime, self.final_datetime)
