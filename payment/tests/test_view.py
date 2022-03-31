625cb263fe6e3496d5bd42511510fbe2bbe329dd
André Luiz S. Vilarinho <andluizv@msn.com>
2022-03-31 09:20:58 -0300

    fix: test payment seller wrong card fields

--- payment/tests/test_view.py
+++ payment/tests/test_view.py
@@ -51,12 +51,12 @@ class PaymentViewTest(APITestCase):
         self.assertEqual(response.data['card_number_info'], "**********3456")
         self.assertNotIn("card_number", response.data)
 
-    def test_create_paymentinfo_seller_403(self):
+    def test_create_paymentinfo_seller_201(self):
         data = {
             "payment_method": "credit",
             "card_number": "12345678123456",
             "cardholders_name": "Teste Resto",
-            "card_expiring_date": "2122-04-01",
+            "card_expiration_date": "2122-04-01",
             "cvv": 456
         }
 
@@ -64,7 +64,7 @@ class PaymentViewTest(APITestCase):
             HTTP_AUTHORIZATION="Token " + self.token_seller)
         response = self.client.post('/api/payment/', data)
 
-        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.status_code, 201)
 
     def test_create_paymentinfo_without_token_401(self):
         data = {
